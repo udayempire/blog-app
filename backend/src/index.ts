6245ffeff1f23867f7300c3874e128e9decc7d1f
udayempire <kuday0726@gmail.com>
2024-10-03 12:20:14 +0530

    added signin

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,38 +2,73 @@ import { Hono } from 'hono'
 
 import { PrismaClient } from "@prisma/client/edge"
 import { withAccelerate } from '@prisma/extension-accelerate'
-import { decode,sign, verify } from 'hono/jwt'
+import { decode, sign, verify } from 'hono/jwt'
 
 const app = new Hono<{
-  Bindings:{
-    DATABASE_URL:string
-    JWT_SECRET:string
+  Bindings: {
+    DATABASE_URL: string
+    JWT_SECRET: string
   }
 }>();
 
-app.post('/api/v1/signup',async (c) => {
+app.use('api/v1/blog/*',async (c,next)=>{
+  const header = c.req.header("authorization") || ""
+  const token = header.split(" ")[1]
+  const response = await verify(token, c.env.JWT_SECRET)
+  if(response.id){
+    next()
+  }
+  else{
+    c.status(403)
+    return c.json({error:"unauthorized"})
+  }
+
+})
+
+app.post('/api/v1/signup', async (c) => {
   const prisma = new PrismaClient({
     datasourceUrl: c.env.DATABASE_URL,
   }).$extends(withAccelerate())
 
   const body = await c.req.json();
-  try{
+  try {
     const user = await prisma.user.create({
-      data:{
-        email:body.email,
-        password:body.password
+      data: {
+        email: body.email,
+        password: body.password
       }
     });
-    const jwt = await sign({id:user.id},c.env.JWT_SECRET)
-    return c.json({jwt})
-  }catch(error){
-    console.error("Error during signup:",error)
+    const jwt = await sign({ id: user.id }, c.env.JWT_SECRET)
+    return c.json({ jwt })
+  } catch (error) {
+    console.error("Error during signup:", error)
     return c.status(403)
   }
 })
 
-app.post('/api/v1/signin', (c) => {
-  return c.text("hello world")
+app.post('/api/v1/signin', async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate())
+
+  const body = await c.req.json();
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        email: body.email,
+        password: body.password
+      }
+    });
+    if (!user){
+      c.status(403);
+      return c.json({error:"user not found"});
+    }
+    const jwt = await sign({id:user.id},c.env.JWT_SECRET)
+    return c.json({jwt});
+  }catch(error){
+    console.error("Error during signin:",error)
+    return c.status(403)
+  }
 })
 
 app.post('/api/v1/blog', (c) => {
