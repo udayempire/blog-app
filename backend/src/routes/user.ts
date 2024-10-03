14338019da81ffe05993748c171a6c26ae879e21
udayempire <kuday0726@gmail.com>
2024-10-03 18:35:23 +0530

    published npm modules

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -2,6 +2,9 @@ import { withAccelerate } from "@prisma/extension-accelerate";
 import { PrismaClient } from "@prisma/client/edge";
 import { Hono } from "hono";
 import { sign, verify } from "hono/jwt"
+import { signupInput } from "@udayempire/blog-common";
+
+
 
 export const userRouter = new Hono<{
     Bindings:{
@@ -17,6 +20,15 @@ userRouter.post('/signup', async (c) => {
     }).$extends(withAccelerate())
 
     const body = await c.req.json();
+    const { success } = signupInput.safeParse(body)
+
+    if(!success){
+        // c.status(411),
+        return c.json({
+            message:"Inputs not correct"
+        })
+    }
+
     try {
         const user = await prisma.user.create({
             data: {
