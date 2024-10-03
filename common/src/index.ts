1e5bc3deb7d1c52184ba247b68be567fa99cb4b9
udayempire <kuday0726@gmail.com>
2024-10-03 18:36:11 +0530

    published npm

---
+++ common/src/index.ts
@@ -0,0 +1,28 @@
+import { z } from "zod"
+export const signupInput = z.object({
+    username: z.string().email(),
+    password: z.string().min(6),
+    name: z.string().optional()
+})
+//type inference in zod
+export type SignupInput = z.infer<typeof signupInput>
+
+export const signinInput = z.object({
+    username:z.string().email(),
+    password:z.string().min(6)
+})
+
+export type SigninInput = z.infer<typeof signinInput>
+//---
+export const createBlog = z.object({
+    title:z.string(),
+    content:z.string()
+}) 
+export type CreateBlog = z.infer<typeof createBlog>
+
+export const updateBlog = z.object({
+    title:z.string(),
+    content:z.string(),
+    id:z.number()
+})
+export type UpdateBlog = z.infer<typeof updateBlog>
