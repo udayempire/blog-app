b28a2ff84bb883c079abaf19aa7adceeb8d650d8
udayempire <kuday0726@gmail.com>
2024-10-12 17:48:56 +0530

    fixed deployement issues

--- frontend/src/pages/Publish.tsx
+++ frontend/src/pages/Publish.tsx
@@ -14,7 +14,7 @@ export const Publish = () => {
             <input type="email" id="helper-text" aria-describedby="helper-text-explanation" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 mb-4" placeholder="Enter The Title of The Blog" onChange={(e) => {
                 setTitle(e.target.value)
             }} />
-            <textarea id="message" rows="20" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500" placeholder="Write your thoughts here..." onChange={(e) => {
+            <textarea id="message" rows={20} className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500" placeholder="Write your thoughts here..." onChange={(e) => {
                 setContent(e.target.value)
             }}>
             </textarea>
